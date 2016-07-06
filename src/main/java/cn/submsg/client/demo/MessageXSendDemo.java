b9f8af0026342471d63e55df84a9b6d0674ca1f7
dogdog7788 <dogdog7788@qq.com>
2016-07-06 18:32:55 +0800

    队列实现及api消息发送测试成功提交

--- src/main/java/cn/submsg/client/demo/MessageXSendDemo.java
+++ src/main/java/cn/submsg/client/demo/MessageXSendDemo.java
@@ -1,7 +1,10 @@
 package cn.submsg.client.demo;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.lang.time.DateFormatUtils;
 
 import cn.submsg.client.util.SubMsgSendUtils;
 
@@ -9,9 +12,13 @@ public class MessageXSendDemo {
 
 	public static void main(String[] args) {
 		String to = "18516632554";
-		String tempId = "MApf82";
+		String tempId = "sUb981";
+		String date = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
 		Map<String,String> param = new HashMap<String,String>();
-		param.put("code", "123658");
+		param.put("name", "潮哥");
+		param.put("numStr", "20");
+		param.put("userName", "dogdog7788");
+		param.put("date", date);
 		SubMsgSendUtils.sendMessage(to, tempId, param);
 	}	
 }
