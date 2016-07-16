1402c890a7fed30d9e3afa92b90582572cd208f7
dogdog7788 <dogdog7788@qq.com>
2016-07-16 11:23:12 +0800

    修复签名逻辑错误

--- src/main/java/cn/submsg/client/config/MessageConfig.java
+++ src/main/java/cn/submsg/client/config/MessageConfig.java
@@ -8,6 +8,6 @@ public class MessageConfig extends AppConfig{
 
 	public static final String APP_ID = "msg_appid";
 	public static final String APP_KEY = "msg_appkey";
-	public static final String APP_SIGNTYPE = "normal";
+	public static final String APP_SIGNTYPE = "msg_signtype";
 	public static final String isOpen = "is_open";
 }
--- src/main/java/cn/submsg/client/demo/MessageXSendDemo.java
+++ src/main/java/cn/submsg/client/demo/MessageXSendDemo.java
@@ -1,16 +1,42 @@
 package cn.submsg.client.demo;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.time.DateFormatUtils;
 
 import cn.submsg.client.util.SubMsgSendUtils;
 
 public class MessageXSendDemo {
 
 	public static void main(String[] args) {
+	
+//		for(int i=0;i<20;i++){
+//			new Thread(new Runnable() {
+//				@Override
+//				public void run() {
+//					while(true){
+//						try {
+//						send();
+//						} catch (Exception e) {
+//							e.printStackTrace();
+//						}
+//						try {
+//							Thread.sleep(500);
+//						} catch (InterruptedException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//					
+//				}
+//			}).start();
+//		}
+		
+		send();
+		
+	}	
+	
+	
+	public static void send(){
 		String to = "15919820372";
 		Map<String,String> param = new HashMap<String,String>();
 //		String tempId = "sUb981";
@@ -29,5 +55,5 @@ public class MessageXSendDemo {
 		param.put("op", "卖出积分");
 		param.put("code", "125356");
 		SubMsgSendUtils.sendMessage(to, tempId, param);
-	}	
+	}
 }
